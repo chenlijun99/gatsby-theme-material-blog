f95b2ce8af518d0304ba6deb7694da79808a3d63
Chen Lijun <chenlijun1999@gmail.com>
2019-09-29 16:09:53 +0200

    chore: convert to gatsby theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,48 @@
-/* eslint-disable @typescript-eslint/camelcase */
-module.exports = {
-  siteMetadata: {
-    title: `Knowledge Base`,
-    description: `My knowledge base`,
-    author: `Lijun Chen`,
-  },
-  plugins: [
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+module.exports = themeOptions => {
+  themeOptions.contentPath = themeOptions.contentPath || 'content/posts';
+  themeOptions.basePath = themeOptions.basePath || '/';
+  return {
+    siteMetadata: {
+      title: `Knowledge Base`,
+      description: `My knowledge base`,
+      author: `Lijun Chen`,
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`,
+    plugins: [
+      `gatsby-plugin-typescript`,
+      `gatsby-plugin-react-helmet`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`,
+        },
       },
-    },
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      `gatsby-plugin-sharp`,
+      `gatsby-transformer-sharp`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: themeOptions.contentPath,
+          path: themeOptions.contentPath,
+        },
       },
-    },
-    `gatsby-plugin-material-ui`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-  ],
+      `gatsby-transformer-remark`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `gatsby-starter-default`,
+          short_name: `starter`,
+          start_url: `/`,
+          background_color: `#663399`,
+          theme_color: `#663399`,
+          display: `standalone`,
+          icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        },
+      },
+      `gatsby-plugin-material-ui`,
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      `gatsby-plugin-offline`,
+    ],
+  }
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,7 @@
-const path = require(`path`);
-
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
 
-  const blogPostTemplate = path.resolve(`src/templates/blogPost.tsx`);
+  const blogPostTemplate = require.resolve(`src/templates/blogPost.tsx`);
 
   const result = await graphql(`
     {
