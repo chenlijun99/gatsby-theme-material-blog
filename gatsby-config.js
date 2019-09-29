1b156511d3dde93360a021e7308026b8b0f98414
Chen Lijun <chenlijun1999@gmail.com>
2019-09-29 23:42:45 +0200

    feat: improve markdown rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,22 @@ module.exports = themeOptions => {
           path: themeOptions.contentPath,
         },
       },
-      `gatsby-transformer-remark`,
+      {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+          plugins: [
+            {
+              resolve: `gatsby-remark-katex`,
+              options: {
+                // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+                strict: `ignore`
+              }
+            },
+            `gatsby-remark-smartypants`,
+            `gatsby-remark-prismjs`,
+          ],
+        },
+      },
       `gatsby-plugin-material-ui`,
     ],
   }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,6 +5,6 @@ export const onRenderBody = ({setHeadComponents}) => {
     <link key="roboto" rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />,
     <link key="material-icon" rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      href="https://fonts.googleapis.com/icon?family=Material+Icons" />,
   ]);
 }
