1d190d7d72ce4bc28c0eef34bf7758920bbec3b1
Chen Lijun <chenlijun1999@gmail.com>
2019-09-29 23:13:49 +0200

    chore: remove gatsby-plugin-{manifest,offline}
    This theme provides only the UI, layout, markdown rendering stuff. It's
    up to the user to decide whether to make their site a PWA.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,22 +27,7 @@ module.exports = themeOptions => {
         },
       },
       `gatsby-transformer-remark`,
-      {
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-          name: `gatsby-starter-default`,
-          short_name: `starter`,
-          start_url: `/`,
-          background_color: `#663399`,
-          theme_color: `#663399`,
-          display: `standalone`,
-          icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-        },
-      },
       `gatsby-plugin-material-ui`,
-      // this (optional) plugin enables Progressive Web App + Offline functionality
-      // To learn more, visit: https://gatsby.dev/offline
-      `gatsby-plugin-offline`,
     ],
   }
 };
