bcd45c323a6a3d94a98513e734757f1e668cf5fa
Chen Lijun <chenlijun1999@gmail.com>
2019-10-29 17:49:33 +0100

    enhancement(home): better UI

--- gatsby-theme-material-blog/src/components/Posts.tsx
+++ gatsby-theme-material-blog/src/components/Posts.tsx
@@ -1,7 +1,10 @@
 import React, { Fragment } from "react";
 import { Link } from "gatsby";
 import Card from "@material-ui/core/Card";
+import Divider from "@material-ui/core/Divider";
+import Box from "@material-ui/core/Box";
 import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
 import Icon from "@material-ui/core/Icon";
 import CalendarIcon from "@material-ui/icons/CalendarToday";
 import Chip from "@material-ui/core/Chip";
@@ -11,40 +14,67 @@ import { PostsQuery } from "../generated/graphql";
 import Typography from "@material-ui/core/Typography";
 
 import SEO from "./SEO";
+import { makeStyles } from "@material-ui/core/styles";
+
+interface PostCardProps {
+  post: PostsQuery["allBlogPost"]["nodes"][0];
+}
+
+const usePostCardStyle = makeStyles(theme => ({
+  headerDate: {
+    ...theme.typography.subtitle2,
+  },
+}));
+
+const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  const classes = usePostCardStyle();
+  return (
+    <Box my={2}>
+      <Card>
+        <CardActionArea component={Link} to={post.slug}>
+          <CardContent>
+            <Box marginBottom={2}>
+              <Typography variant="h5" component="h2" noWrap>
+                {post.title}
+              </Typography>
+              <Box display="flex" alignItems="center" alignContent="center">
+                <CalendarIcon className={classes.headerDate} />
+                <Typography variant="subtitle2" noWrap>
+                  {post.date}
+                </Typography>
+              </Box>
+            </Box>
+            <Divider />
+            <Box my={2}>
+              <Typography variant="body1">{post.excerpt}</Typography>
+            </Box>
+            <Divider />
+            <Box marginTop={2}>
+              {post.tags.map((tag, index) => {
+                return (
+                  <Chip
+                    key={typeof tag === "string" ? tag : index}
+                    label={tag}
+                  ></Chip>
+                );
+              })}
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Box>
+  );
+};
 
 const Posts: React.FC<{ data: PostsQuery }> = ({ data }) => {
   return (
     <React.Fragment>
       <SEO title="Home" />
-      <LayoutContext.Consumer>
-        {({ setHeaderProps }) => {
-          return data.allBlogPost.nodes.map(node => {
-            const title = node.title || node.slug;
-            const tags = node.tags || [];
-            return (
-              <Card key={node.slug}>
-                <CardActionArea component={Link} to={node.slug}>
-                  <Typography variant="h5" component="h2" noWrap>
-                    {title}
-                  </Typography>
-                  <Typography variant="subtitle1" noWrap>
-                    {node.date}
-                  </Typography>
-                  <Typography variant="body1">{node.excerpt}</Typography>
-                </CardActionArea>
-                {tags.map((tag, index) => {
-                  return (
-                    <Chip
-                      key={typeof tag === "string" ? tag : index}
-                      label={tag}
-                    ></Chip>
-                  );
-                })}
-              </Card>
-            );
-          });
-        }}
-      </LayoutContext.Consumer>
+      <Box mx={[0, 10]}>
+        {data.allBlogPost.nodes.map(node => {
+          return <PostCard key={node.slug} post={node} />;
+        })}
+      </Box>
     </React.Fragment>
   );
 };
